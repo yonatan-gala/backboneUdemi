9c5194f3685c6eabceb9d6d3a3fe036a771860cd
yonatan.levari <GyoGyo75>
2018-10-31 16:12:06 +0200

    add feature task description add task code.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
+var Vehicle = Backbone.Model.extend({
+    idAttribue: "songId",
+    urlRoot: "api/vehicles",
+    defaults: {
+        registrationNumber: "000000"
+    },
+    start: function () {
+        console.log("Vehicle started");
+    },
+    validate: function (objectAttributes) {
+        if (!objectAttributes.registrationNumber) {
+            return "registrationNumber is required"
+        }
+    }
+});
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+var Car = Vehicle.extend({
+    start: function () {
+        console.log("Car with registration number " + this.attributes.registrationNumber + " started");
+    }
+});
+
+var newCar = new Car({
+    registrationNumber: "XLI887",
+    color: "Blue"
+});
